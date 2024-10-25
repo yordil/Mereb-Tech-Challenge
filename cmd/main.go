8b5627b27ab6f0c3da544f704e0652f6e505a57e
yordanos legesse <yordanoslegesse15@gmail.com>
2024-10-25 15:34:55 +0300

    basic functionality ended

---
+++ cmd/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/yordil/mereb-tech-challenge/internal/route"
+)
+
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// allowed to accept  a request from every origin
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		
+		// Handle OPTIONS requests for preflight checks
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusOK)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func main() {
+
+	router := gin.Default()
+	// Add CORS middleware
+	router.Use(CORSMiddleware())
+	
+	route.SetupAllRoutes(router)
+
+	// Configure the HTTP server with the router and start the server
+	server := &http.Server{
+		Addr:    ":8080",       
+		Handler: router,
+	}
+
+
+
+	// Start server in a goroutine to handle graceful shutdown and prevent memory leak
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe(): %s", err)
+		}
+	}()
+
+	
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("Shutting down server...")
+
+	
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	log.Println("Server exiting")
+}
